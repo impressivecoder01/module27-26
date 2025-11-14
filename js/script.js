f01cb613a2ce83b35b54b88bdc56173c1c248d33
impressivecoder1 <rohanrahman251@gmail.com>
2025-11-15 01:53:03 +0600

    class four

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 // login button function
 document.getElementById('login-btn').addEventListener('click',function(event){
     event.preventDefault()
-    console.log(event)
+    const  mobileNumber = 11111111111;
+    const pin = 2111;
+    const mobileNumberValue = parseInt(document.getElementById('mobile-number').value) 
+    const pinNumberValue = parseInt(document.getElementById('pin-number').value)
+    
+    if(mobileNumber === mobileNumberValue && pin=== pinNumberValue){
+        window.location.href= "home.html"
+    }
+    else{
+        alert('Invalid Credentials')
+    }
 })
