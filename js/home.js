ac2dd7af0ed8b9a419e79dfefb5bdb3b9ccfcd41
impressivecoder1 <rohanrahman251@gmail.com>
2025-11-20 02:31:19 +0600

    pin added

--- js/home.js
+++ js/home.js
@@ -1,11 +1,23 @@
 document.getElementById('add-money-btn').addEventListener('click', function(event){
     event.preventDefault();
+    const validPin = 2111;
     const bank = document.getElementById('bankName').value;
     const accountNumber = document.getElementById('accountNumber').value;
     const amount = parseInt(document.getElementById('addAmount').value);
-    const pin = document.getElementById('addPin').value;
+    const pin = parseInt(document.getElementById('addPin').value);
     const availableBalance = parseInt(document.getElementById('available-balance').innerText);
-    const totalNewAvailableBalance = amount + availableBalance;
-    console.log(totalNewAvailableBalance)
+    if(accountNumber.length < 11){
+        alert('Provide a valid account number')
+        return;
+    }
+    if(pin === validPin){
+        const totalNewAvailableBalance = amount + availableBalance;
+    // console.log(totalNewAvailableBalance)
     document.getElementById('available-balance').innerText = totalNewAvailableBalance;
+    }
+    else{
+        alert('provide a valid pin please')
+    }
+    
+    
 })
