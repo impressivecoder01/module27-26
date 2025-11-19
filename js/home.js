9cd7a719bc87610ede779c81d9ad447de8eecf0e
impressivecoder1 <rohanrahman251@gmail.com>
2025-11-20 01:51:31 +0600

    money added class six

--- js/home.js
+++ js/home.js
@@ -1,4 +1,11 @@
 document.getElementById('add-money-btn').addEventListener('click', function(event){
     event.preventDefault();
-    
+    const bank = document.getElementById('bankName').value;
+    const accountNumber = document.getElementById('accountNumber').value;
+    const amount = parseInt(document.getElementById('addAmount').value);
+    const pin = document.getElementById('addPin').value;
+    const availableBalance = parseInt(document.getElementById('available-balance').innerText);
+    const totalNewAvailableBalance = amount + availableBalance;
+    console.log(totalNewAvailableBalance)
+    document.getElementById('available-balance').innerText = totalNewAvailableBalance;
 })
